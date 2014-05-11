a3d52bdcf17fb00c35d67347b7697405fad5d2a1
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-05-10 18:46:31 -0600

    Changes from today plus draggable stuff

--- activities/js/controllers/alphabetize.js
+++ activities/js/controllers/alphabetize.js
@@ -1,19 +1,16 @@
 angular.module('ActivitiesApp')
 	.controller('AlphabetizeController', function($scope) {
 		$scope.words = ['Cat', 'Hat', 'Mat', 'Rat', 'Bat', 'Sat', 'Pat', 'Fat', 'Vat'];
-		shuffle($scope.words);
-		function shuffle(o){ //v1.0
-    		for(var j, x, i = o.length; i; j = Math.floor(Math.random()
-    			 * i), x = o[--i], o[i] = o[j], o[j] = x);
-    			return o;
-			};
-	})
+    $scope.shuffle = function(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random()
+           * i), x = o[--i], o[i] = o[j], o[j] = x);
+          return o;
+    };
+		$scope.shuffle($scope.words);
 
-
- 	$(function() {
-		 $( "#sortable" ).sortable();
-		 $( "#sortable" ).disableSelection();
-	 });
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+	});
 
 
 
